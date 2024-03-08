3ebdcc0751e6976169bb1474404d63d2d76b098e
kavvata <gabriel.kawata@gmail.com>
2024-03-08 19:29:29 -0300

    feat: update e destroy de CursoController

--- Docker/start-project/app/Http/Controllers/CursoController.php
+++ Docker/start-project/app/Http/Controllers/CursoController.php
@@ -30,7 +30,7 @@ class CursoController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): void
     {
         //
     }
@@ -62,7 +62,7 @@ class CursoController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(string $id): void
     {
         $curso = $this->repository->findById($id);
     }
@@ -70,7 +70,7 @@ class CursoController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(string $id): void
     {
         //
     }
@@ -78,16 +78,43 @@ class CursoController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, string $id): string
     {
-        //
+        $curso = $this->repository->findById($id);
+
+        if (!isset($curso)) {
+            return '<h1>Update - Curso not found!</h1>';
+        }
+
+        $eixo = (new EixoRepository)->findById($request->eixo_id);
+        $nivel = (new NivelRepository)->findById($request->nivel_id);
+
+        if (!isset($eixo) || !isset($nivel)) {
+            return '<h1>Update - Eixo or Nivel not found!</h1>';
+        }
+
+        $curso->nome = strtoupper($request->nome, 'UTF-8');
+        $curso->sigla = strtoupper($request->nome, 'UTF-8');
+        $curso->total_horas = $request->horas;
+
+        $curso->eixo()->associate($eixo);
+        $curso->nivel()->associate($nivel);
+        $this->repository->save($curso);
+
+        return '<h1>Update - OK!</h1>';
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $id): string
     {
-        //
+        $isSuccess = $this->repository->delete($id);
+
+        if (!$isSuccess) {
+            return '<h1>Delete - Curso not found!</h1>';
+        }
+
+        return '<h1>Delete - OK!</h1>';
     }
 }
