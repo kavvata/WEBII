f036331cbdc22a1e0e896e8258642026f83e5e2f
kavvata <gabriel.kawata@gmail.com>
2024-03-10 18:21:38 -0300

    feat: relacao entre modelos Curso e Turma

--- Docker/start-project/app/Models/Curso.php
+++ Docker/start-project/app/Models/Curso.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -20,5 +21,10 @@ class Curso extends Model
     public function nivel(): BelongsTo
     {
         return $this->belongsTo('\App\Models\Nivel');
+    }
+
+    public function turmas(): HasMany
+    {
+        return $this->hasMany(Turma::class);
     }
 }
