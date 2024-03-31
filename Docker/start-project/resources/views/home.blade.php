c188c8ff614a346f5752e8d845b2d0ef1c98bb52
kavvata <gabriel.kawata@gmail.com>
2024-03-30 21:25:01 -0300

    feat: nova view para index

--- Docker/start-project/resources/views/home.blade.php
+++ Docker/start-project/resources/views/home.blade.php
@@ -1,23 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>SIGAC - Home</title>
-        <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}"/>
-        <link rel="stylesheet" href="{{asset('css/all.min.css')}}"/>
-        <link rel="stylesheet" href="{{asset('css/style.css')}}"/>
-    </head>
-    <body>
-        <header>
-            <a href="#">SIGAC</a>
-            <ul id="header-links" class="nav nav-pills d-inline-flex">
-                <li class="nav-item">FOO</li>
-                <li class="nav-item">BAR</li>
-            </ul>
-        </header>
-        <div>
-            <h1>Oi mae!</h1>
-            <!-- Do what you can, with what you have, where you are. - Theodore Roosevelt -->
-        </div>
-    </body>
+@extends('templates/main', ['titulo'=>'Home'])
+
+@section('conteudo')
+<h1>Home</h1>
+@endsection
